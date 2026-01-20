6a7e51071c3030d6527a51097b5619a73169592b
gabrielrobotic <gabrielrobotic@gmail.com>
2026-01-20 15:42:02 +0000

    add: dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }: {
+  channel = "stable-25.05";
+  packages = [
+    pkgs.zsh
+  ];
+  env = { };
+  idx = {
+    extensions = [
+      "google.gemini-cli-vscode-ide-companion"
+      "angular.ng-template"
+      "bradlc.vscode-tailwindcss"
+    ];
+    workspace = {
+      onCreate = {
+        ohmyzsh-setup = ''
+          echo "Verificando Oh My Zsh..."
+
+          OHMYZSH="$HOME/.oh-my-zsh"
+          ZSHRC="$HOME/.zshrc"
+
+          if [ ! -d "$OHMYZSH" ]; then
+            echo "Oh My Zsh não encontrado. Instalando..."
+
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+            echo "Configurando tema 'jonathan'..."
+            sed -i 's/ZSH_THEME=".*"/ZSH_THEME="jonathan"/' "$ZSHRC"
+
+            echo "Inserindo ZSH_DISABLE_COMPFIX=true..."
+            sed -i '/^source \$ZSH\/oh-my-zsh.sh/i ZSH_DISABLE_COMPFIX=true' "$ZSHRC"
+        
+          fi
+        '';
+      };
+      onStart = { };
+    };
+  };
+}
